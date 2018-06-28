8223c9d4866442099274199caddc69834c99e1d8
Scott Carchedi <scott@envuestudios.com>
2018-06-28 13:25:52 -0500

    Improvements to right  sidebar

--- resources/views/partials/controlbar.blade.php
+++ resources/views/partials/controlbar.blade.php
@@ -9,21 +9,7 @@
         padding: 7px 10px;
     }
 </style>
-<script>
-    $(document).ready(function(){
-        // Add minus icon for collapse element which is open by default
-        $(".collapse.in").each(function(){
-        	$(this).siblings(".panel-heading").find(".glyphicon").addClass("glyphicon-minus").removeClass("glyphicon-plus");
-        });
-        
-        // Toggle plus minus icon on show hide of collapse element
-        $(".collapse").on('show.bs.collapse', function(){
-        	$(this).parent().find(".glyphicon").removeClass("glyphicon-plus").addClass("glyphicon-minus");
-        }).on('hide.bs.collapse', function(){
-        	$(this).parent().find(".glyphicon").removeClass("glyphicon-minus").addClass("glyphicon-plus");
-        });
-    });
-</script>
+
 <!--control sidebar -->
 <aside class="control-sidebar control-sidebar-light">
     <!-- Create the tabs -->
