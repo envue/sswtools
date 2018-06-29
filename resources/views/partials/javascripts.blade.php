abeacc891fd6f9597f350d0d1e97087e7279d0f9
Scott Carchedi <scott@envuestudios.com>
2018-06-29 10:27:47 -0500

    Added user guiding script

--- resources/views/partials/javascripts.blade.php
+++ resources/views/partials/javascripts.blade.php
@@ -80,4 +80,20 @@
     });
 </script>
 
+<!-- User Guiding script -->
+<script>
+(function (u, s, e, r, g) { 
+    u[r] = u[r] || [];
+    u[r].push({
+      'ug.start': new Date().getTime(), event: 'embed.js',
+    });
+    var f = s.getElementsByTagName(e)[0],
+        j = s.createElement(e);
+    j.async = true;
+    j.src = 'https://static.userguiding.com/media/user-guiding-'
+     + g + '-embedded.js';
+    f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'userGuidingLayer', '63239968ID');
+</script>
+
 @yield('javascript')
