66f3836927899d37e998b82e6f7bf855109a4931
Scott Carchedi <scott@envuestudios.com>
2018-07-20 13:49:24 -0500

    moved user guiding script to app black

--- resources/views/partials/javascripts.blade.php
+++ resources/views/partials/javascripts.blade.php
@@ -82,21 +82,4 @@
         });
     });
 </script>
-
-<!-- User Guiding script -->
-<script>
-(function (u, s, e, r, g) { 
-    u[r] = u[r] || [];
-    u[r].push({
-      'ug.start': new Date().getTime(), event: 'embed.js',
-    });
-    var f = s.getElementsByTagName(e)[0],
-        j = s.createElement(e);
-    j.async = true;
-    j.src = 'https://static.userguiding.com/media/user-guiding-'
-     + g + '-embedded.js';
-    f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'userGuidingLayer', '63239968ID');
-</script>
-
 @yield('javascript')
