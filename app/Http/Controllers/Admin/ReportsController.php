106ab5597abb5bb891872f430b34cf41643e67cd
Scott Carchedi <scott@envuestudios.com>
2018-11-03 09:49:28 -0500

    updated month-year format

--- app/Http/Controllers/Admin/ReportsController.php
+++ app/Http/Controllers/Admin/ReportsController.php
@@ -15,10 +15,10 @@ class ReportsController extends Controller
 
         $results = User::get()->sortBy('created_at')->groupBy(function ($entry) {
             if ($entry->created_at instanceof \Carbon\Carbon) {
-                return \Carbon\Carbon::parse($entry->created_at)->format('Y-m');
+                return \Carbon\Carbon::parse($entry->created_at)->format('m-Y');
             }
 
-            return \Carbon\Carbon::createFromFormat(config('app.date_format'), $entry->created_at)->format('Y-m');
+            return \Carbon\Carbon::createFromFormat(config('app.date_format'), $entry->created_at)->format('m-Y');
         })->map(function ($entries, $group) {
             return $entries->count('id');
         });
