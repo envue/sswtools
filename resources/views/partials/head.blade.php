8115a2e2a622f60840c54913d3dd50dc34347245
Scott Carchedi <scott@envuestudios.com>
2020-08-10 12:36:32 -0500

    added hotjar

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -49,3 +49,15 @@
 (function(d,h,w){var gist=w.gist=w.gist||[];gist.methods=['trackPageView','identify','track','setAppId'];gist.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);gist.push(e);return gist;}};for(var i=0;i<gist.methods.length;i++){var c=gist.methods[i];gist[c]=gist.factory(c)}s=d.createElement('script'),s.src="https://widget.getgist.com",s.async=!0,e=d.getElementsByTagName(h)[0],e.appendChild(s),s.addEventListener('load',function(e){},!1),gist.setAppId("arxqgrv9"),gist.trackPageView()})(document,'head',window);
 </script>
 <!-- end Gist JS code-->
+
+<!-- Hotjar Tracking Code for https://app.schoolsocialwork.net -->
+      <script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1939163,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
